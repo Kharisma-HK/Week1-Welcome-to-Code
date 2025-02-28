2a31bc7e64fcbfceb8ce751bca9cd8c1d9521e55
Kharisma-HK <rhago3028@gmail.com>
2025-02-28 08:18:45 +0700

    Create loop1.js sama loop2.js kebalik bg sori

---
+++ quiz/part3/loop2.js
@@ -0,0 +1,18 @@
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax for. 
+// Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan 
+// menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+//tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua 
+i = 0;
+while(i < 10){
+console.log('looping MAJU ' + i)
+i++
+}
+
+console.log()
+
+let j = 10;
+while( j > 0){
+  console.log(' looping mundur' + j)
+  j--
+}

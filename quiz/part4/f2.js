df207e7eb345c79a1833f61860cbe4d1de4081b9
Kharisma-HK <rhago3028@gmail.com>
2025-03-02 06:51:26 +0700

    Create f2.js

---
+++ quiz/part4/f2.js
@@ -0,0 +1,14 @@
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari 
+// dua parameter yang dikirim.
+
+//bikin fungsinya disini
+
+let num1 = 1
+let num2 = 2
+
+function calculateMultiply(num1, num2){
+  let hasilPerkalian = num1 * num2
+  return hasilPerkalian
+}
+
+console.log(calculateMultiply(num1, num2));
